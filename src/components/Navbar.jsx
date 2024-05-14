e53754b311c3538d6b386f09adb09ab9a668b01a
Dipanshu Pandey <dps.coders@gmail.com>
2024-05-14 15:05:46 +0530

    Dark mode feature added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-
+import day from './Loader/day-mode.png'
+import night from './Loader/night-mode.png'
 
 export default function Navbar() {
+
+  const [darkMode,setDarkMode]= useState(false);
+
   return (
-    <div>
-      <nav className='flex gap-2'>
-      <Link to='/'>Home</Link>
-      <Link to='/about'>About</Link>
-      <Link to='/contact'>Contact</Link>
-    </nav>
+    <div><nav className={`p-4 text-xl flex px-10  ${darkMode ? 'bg-[#9484ff] text-black' : 'bg-black text-white'} justify-between ` } >
+      
+        <div className='flex gap-6'>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/contact'>Contact</Link>
+        </div>
+  
+       <div className="ToggleBox flex gap-3">
+          <div className=" DAY h-8 w-8 contrast-200 saturate-200"><img src= {day} alt="" /></div>
+             <label className="inline-flex items-center cursor-pointer border-white  rounded-full">
+                 <input type="checkbox" value="" className="sr-only peer" onClick={()=>setDarkMode((prev)=>!prev)} />
+                 <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+             </label>
+          <div className=" NIGHT h-8 w-8  contrast-200 saturate-200"><img src= {night} alt="" /></div>  
+       </div>
+
+      </nav>
     </div>
   )
 }
